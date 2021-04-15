6acda97198fdb86a3eae06212aadf4d54877dc3d
Camilo Guzman <camiloge_91@hotmail.com>
2021-04-15 09:58:47 -0500

    responsive_2

--- src/components/InfoGuide.js
+++ src/components/InfoGuide.js
@@ -7,7 +7,7 @@ const InfoGuide = () => {
       <div className="container__g">
         <h2>Información preliminar</h2>
         <p className="event-description">Esta guía está basada en mi experiencia durante la asistencia al festival en el año 2018, así que, los precios y planes podrían variar a medida que transcurren los años. Así mismo, 
-        a continuación se describirá según <strong>mi experiencia</strong> los pasos que seguí, según lo que consideré más conveniente. Para más información, ingresar a la página <a class="link" href="https://www.tomorrowland.com/global/">www.tomorrowland.com/global/</a> o
+        a continuación se describirá según <strong>mi experiencia</strong> los pasos que seguí, según lo que consideré más conveniente. Para más información, ingresar a la página <a className="link" href="https://www.tomorrowland.com/global/">www.tomorrowland.com/global/</a> o
         contactarme por medio de mis redes sociales, ubicadas en la parte inferior de la presente página.</p> 
       </div>
     </div>
