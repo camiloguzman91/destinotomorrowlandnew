bcccb74eb6faec999a4c721ebfe032683024fec9
Camilo Guzman <camiloge_91@hotmail.com>
2021-10-06 18:17:25 -0500

    tomorrowland_link

--- src/components/Video_g.js
+++ src/components/Video_g.js
@@ -1,26 +1,29 @@
 import React from 'react'
 
 import '../assets/styles/components/Video.css'
+import video1 from  '../assets/videos/a_1.mp4'
+import video2 from  '../assets/videos/b_1.mp4'
+import video3 from  '../assets/videos/c_1.mp4'
 
 const Video_g = () => {
   return (
     <div>
       <section className="post-list">
-        <a href="" className="post">
+        <div className="post">
           <figure className="post-image">
-            <video className="html-video" src="Imágenes/a_1.mp4" width="350" height="293" controls></video>
+            <video className="html-video" src={video1} width="350" height="293" controls type="video/mp4"/>
           </figure>
-        </a>
-        <a href="" className="post">
+        </div>
+        <div className="post">
           <figure className="post-image">
-            <video className="html-video" src="Imágenes/b_1.mp4" width="350" height="293" controls></video>
+            <video className="html-video" src={video2} width="350" height="293" controls></video>
           </figure>
-        </a>
-        <a href="" className="post">
+        </div>
+        <div className="post">
           <figure className="post-image">
-            <video className="html-video" src="Imágenes/c_1.mp4" width="350" height="293" controls></video>
+            <video className="html-video" src={video3} width="350" height="293" controls></video>
           </figure>
-        </a>
+        </div>
       </section>
     </div>
   )
